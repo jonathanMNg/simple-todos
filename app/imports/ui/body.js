b58554a67a2e1a766cf5a57b7477254f45d77757
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-10-10 23:09:30 -1000

    Task 4 Resubmit
    Resubmit(Task 4: Collections, Temporary UI State, Adding user accounts)

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -2,24 +2,16 @@ import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
+import { Meteor } from 'meteor/meteor';
+
 import './task.js';
 
 import './body.html';
 
-import { ReactiveDict } from 'meteor/reactive-dict';
-
 Template.body.helpers({
   tasks() {
-    const instance = Template.instance();
-    if (instance.state.get('hideCompleted')) {
-      // If hide completed is checked, filter tasks
-      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
-    }
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
-  incompleteCount() {
-    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -36,16 +28,11 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     // Clear form
     target.text.value = '';
   },
-  'change .hide-completed input'(event, instance) {
-    instance.state.set('hideCompleted', event.target.checked);
-  },
-});
-
-Template.body.onCreated(function bodyOnCreated() {
-  this.state = new ReactiveDict();
 });
